d059dd59ff81a13ab0c5ed3b859fde8191cd2b0d
RC <i.gooner168@gmail.com>
2021-06-21 16:29:28 +0530

    customize webpack config and package.json for prod and dev build

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export function App() {
     return (
         <div>
             <h1>Welcome { new Date().toString()}</h1>
-            <h2>Welcome to React app bootstraps using Webpack 5</h2>
+            <h2>Welcome to React app bootstraps using Webpack</h2>
         </div>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development'
 
 module.exports = {
     //entry : './src/index.js',
+    mode:mode,
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'index.bundle.js'
     },
     devServer: {
-        port:3010,
+        port:8081,
         watchContentBase: true,
     },
     module: {
